c3c8ff913cbb12137e1f56e603b1b1f56c1df176
Daniel Lupastean <lupasteandaniell1@gmail.com>
2021-04-02 15:16:17 +0300

    some modifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,21 @@
 const menuBtn = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
-const dashboard = document.querySelector('.content');
+const content = document.querySelector('.content');
+
 let menuOpen = false;
+
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
         sidebar.classList.add('shown');
-        dashboard.style.filter = "brightness(40%)";
+        content.style.filter = "brightness(40%)";
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         sidebar.classList.remove('shown');
-        dashboard.style.filter = "brightness(100%)";
+        content.style.filter = "brightness(100%)";
         menuOpen = false;
     }
 });
+
+
